e7c864315a5e75d95b0a3e4adaa5620af55b87a5
costeamihai23 <costeamihai18@stud.ase.ro>
2020-12-27 02:47:25 +0200

    sss

--- app/src/main/java/com/example/livescore_app/MainActivity.java
+++ app/src/main/java/com/example/livescore_app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
+                            Intent i=new Intent(getApplicationContext(),Meciuri.class);
+                            startActivity(i);
                         }
                     });
                     AlertDialog alertDialog=builder.create();
---
+++ app/src/main/java/com/example/livescore_app/Meciuri.java
@@ -0,0 +1,14 @@
+package com.example.livescore_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Meciuri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_meciuri);
+    }
+}
