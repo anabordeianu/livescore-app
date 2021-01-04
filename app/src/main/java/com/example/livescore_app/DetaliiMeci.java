e74d2dce1ceffa78b50ce01ce35f4366e6a787d4
costeamihai23 <costeamihai18@stud.ase.ro>
2021-01-04 16:28:24 +0200

    Update detalii meci, preluare date pentru detaliile meciurilor

--- app/src/main/java/com/example/livescore_app/DetaliiMeci.java
+++ app/src/main/java/com/example/livescore_app/DetaliiMeci.java
@@ -4,9 +4,20 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Notification;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class DetaliiMeci extends AppCompatActivity {
 
@@ -39,6 +50,61 @@ public class DetaliiMeci extends AppCompatActivity {
         mData_tv=findViewById(R.id.dataMeci_tv);
 
         mData_tv.setText(data); 
+        
+        loadData();
+    }
+
+    private void loadData() {
+        //bara de progres care va fi afisata in timp ce sunt primite datele meciurilor
+        final ProgressDialog pd=new ProgressDialog(this);
+        pd.setMessage("Meciurile sunt incarcate...");
+        pd.show();
+
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+            pd.dismiss();
+
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                String gazde = jsonObject.getString("team-1");
+                String oaspeti = jsonObject.getString("team-2");
+                String statusMeci = jsonObject.getString("matchStarted");
+                if(statusMeci.equals("true")){
+                    statusMeci="Meciul a inceput";
+                }
+                else{
+                    statusMeci="Meciul nu a inceput";
+                }
+                mEchipa1_tv.setText(gazde);
+                mEchipa2_tv.setText(oaspeti);
+                mStatusMeci_tv.setText(statusMeci);
+                try {
+                    //aceste valori vor fi primite doar daca meciul a inceput
+                    //asa ca trebuie trecute intr-un try catch separat
+                    String scor=jsonObject.getString("score");
+                    String descriere=jsonObject.getString("description");
+                    mScor_tv.setText(scor);
+                    mDescriere_tv.setText(descriere);
+                }
+                catch(Exception e){
+                    Toast.makeText(DetaliiMeci.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch (Exception e){
+                Toast.makeText(DetaliiMeci.this,""+e.getMessage() ,Toast.LENGTH_SHORT).show();
+            }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(DetaliiMeci.this,"Ai eroare, pariorule: "+error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     @Override
